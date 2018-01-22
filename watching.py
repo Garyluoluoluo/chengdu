f20e199ebda990694be14d3a4b3600cae57f456f
Garyluoluoluo <506494050@qq.com>
2018-01-23 02:28:24 +0800

    Update watching.py

--- watching.py
+++ watching.py
@@ -4,15 +4,7 @@ import http.cookiejar
 from bs4 import BeautifulSoup
 import time
 url = 'http://www.cdfangxie.com/Infor/type/typeid/36.html'
-#直接通过url来获取网页数据  
 
-
-# for child in soup.descendants:
-#     print(child)
-#
-# for string in soup.stripped_strings:
-#     print(repr(string))
-#print(soup.find_all("a", target="_blank"))
 
 def checkhouse(url):
     house_lis = []
@@ -27,6 +19,8 @@ def checkhouse(url):
         house = tag.string.split("|")
         house_lis.append(house)
     return house_lis
+
+
 houselist1 = []
 i = 0
 while True:
@@ -43,13 +37,4 @@ while True:
     houselist1 = houselist2
     time.sleep(2)
     i += 1
-    #print(house_dic)
-#print(soup.div)
-
-# while True:
-#     x = input( "查询当前编号：" )
-#     if int(x) in range(len(house_lis)):
-#        print(house_lis[int(x)])
-#     else:
-#         x = input("请输入正确编号")
 
