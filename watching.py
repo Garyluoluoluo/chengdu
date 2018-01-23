058ae675de155fe9bd4b368895e236a5f534e918
Garyluoluoluo <506494050@qq.com>
2018-01-23 13:54:09 +0800

    Add files via upload

--- watching.py
+++ watching.py
@@ -2,6 +2,15 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 import time
+import requests
+import json
+from wxpy import *
+import csv
+from time import strftime
+
+bot = Bot()
+bot.enable_puid() #启用聊天对象的puis属性
+my_friend = bot.friends().search('人类的好朋友')[0]
 
 url = 'http://www.cdfangxie.com/Infor/type/typeid/36.html'
 
@@ -25,13 +34,18 @@ if __name__ == '__main__':
     while True:
         houselist2 = checkhouse(url)
         if houselist1 == houselist2:
-            print('没有更新数据'+str(houselist1[0]))
+            print('没有更新数据'+str(houselist2[0]))
+            my_friend.send('没有更新数据'+str(houselist2[0]))
         else:
             if i > 0 :
                 print("新增楼盘>>>>"+ str(houselist2[0]))
+                my_friend.send("新增楼盘>>>>"+ str(houselist2[0]))
+            elif i == 0:
+                print('启动成功，当前楼盘列表:'+str(houselist2))
+                my_friend.send('启动成功，当前楼盘列表:'+str(houselist2))
         if i > 100:
             i = 1
         houselist1 = houselist2
-        time.sleep(600)
+        time.sleep(60)
         i += 1
 
