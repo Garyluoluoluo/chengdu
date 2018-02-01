9d1a30bfc9b8cea60782f3dfdd1199827be99b41
Garyluoluoluo <506494050@qq.com>
2018-02-01 12:47:12 +0800

    Add files via upload

--- watching.py
+++ watching.py
@@ -8,9 +8,20 @@ from wxpy import *
 import csv
 from time import strftime
 
-bot = Bot()
-bot.enable_puid() #启用聊天对象的puis属性
-my_friend = bot.friends().search('人类的好朋友')[0]
+wechat = input('是否启用微信通知功能： Y/N ？')
+if wechat == 'y' or wechat == 'Y':
+	wechat = 1
+else:
+	wechat = 0
+
+if wechat :
+
+    watcher = input('请输入微信名称:' )  #需要通知的watcher 微信名
+    bot = Bot()
+    bot.enable_puid() #启用聊天对象的puis属性
+    my_friend = bot.friends().search(watcher)[0]
+sleeptime = 1800        # 单位为秒
+
 
 url = 'http://www.cdfangxie.com/Infor/type/typeid/36.html'
 
@@ -35,17 +46,20 @@ if __name__ == '__main__':
         houselist2 = checkhouse(url)
         if houselist1 == houselist2:
             print('没有更新数据'+str(houselist2[0]))
-            my_friend.send('没有更新数据'+str(houselist2[0]))
+            if wechat :
+                my_friend.send('没有更新数据'+str(houselist2[0]))
         else:
             if i > 0 :
                 print("新增楼盘>>>>"+ str(houselist2[0]))
-                my_friend.send("新增楼盘>>>>"+ str(houselist2[0]))
+                if wechat :
+                    my_friend.send("新增楼盘>>>>"+ str(houselist2[0]))
             elif i == 0:
                 print('启动成功，当前楼盘列表:'+str(houselist2))
-                my_friend.send('启动成功，当前楼盘列表:'+str(houselist2))
+                if wechat :
+                    my_friend.send('启动成功，当前楼盘列表:'+str(houselist2))
         if i > 100:
             i = 1
         houselist1 = houselist2
-        time.sleep(60)
+        time.sleep(sleeptime)
         i += 1
 
